9bb2bb3c8708a2491e1a7616a85a836ff876f51e
jalmanro <jesse.almanrode@twtelecom.com>
2015-07-13 11:43:29 -0600

    Adding setup.py file and removing version identifier from module.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# coding=utf-8
+"""Setup file for infoblox module"""
+
+from distutils.core import setup
+
+
+setup(name='infoblox',
+      version='1.4.5',
+      description='Python Infoblox WAPI Module',
+      author='Jesse Almanrode',
+      author_email='jesse@almanrode.com',
+      url='https://bitbucket.org/isaiah1112/infoblox',
+      py_modules=['infoblox'],
+      license='GNU Lesser General Public License v3 or later (LGPLv3+)',
+      install_requires=['simplejson>=3.6.5',
+                        'requests>=2.7.0',
+                        ],
+      platforms='any',
+      classifiers=[
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
+          'Development Status :: 5 - Production/Stable',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      )
