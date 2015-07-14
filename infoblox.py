30d79cd655e5c431f701657996cb95a571aeb6e3
Jesse Almanrode <isaiah1112@gmail.com>
2015-07-14 17:47:00 -0600

    Moved __version__ back into setup.py

--- infoblox.py
+++ infoblox.py
@@ -15,7 +15,6 @@ requests module as well as numerous shortcuts for manipulating objects within In
 #
 #     You should have received a copy of the GNU Lesser General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = '1.4.6'
 
 import requests
 import json
