9b4033cdfabdc90cafe14afee69183cd47c04c40
Jesse Almanrode <jesse@almanrode.com>
2025-06-06 09:22:09 -0600

    Updated errors in unittests

--- test/test_iblox.py
+++ test/test_iblox.py
@@ -2,9 +2,7 @@
 # coding=utf-8
 """Integration and unit Tests for iblox Python Module"""
 from __future__ import print_function
-import os
 import requests_mock
-import sys
 import unittest
 import warnings
 from builtins import str
@@ -12,8 +10,6 @@ from builtins import str
 
 __author__ = 'Jesse Almanrode'
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_root)
 import iblox
 
 
@@ -38,8 +34,8 @@ class Testiblox(unittest.TestCase):
         """
         result = iblox.ipv4addr_obj('192.168.0.1', configure_for_dhcp=True)
         self.assertTrue(isinstance(result, dict))
-        self.assertEquals(result['configure_for_dhcp'], True)
-        self.assertEquals(result['ipv4addr'], '192.168.0.1')
+        self.assertEqual(result['configure_for_dhcp'], True)
+        self.assertEqual(result['ipv4addr'], '192.168.0.1')
         pass
 
     def test_get(self, mock_adapter):
@@ -48,9 +44,9 @@ class Testiblox(unittest.TestCase):
         mock_adapter.get(requests_mock.ANY, json=[{'_ref': 'view/ZG5zLnZpZXckLl9kZWZhdWx0:default/true', 'is_default': True, 'name': 'default'}])
         result = self.iblox_conn.get(objtype='view', name='default')
         self.assertTrue(isinstance(result, list))
-        self.assertEquals(len(result), 1)
+        self.assertEqual(len(result), 1)
         self.assertTrue(isinstance(result[0], dict))
-        self.assertEquals(result[0]['_ref'], 'view/ZG5zLnZpZXckLl9kZWZhdWx0:default/true')
+        self.assertEqual(result[0]['_ref'], 'view/ZG5zLnZpZXckLl9kZWZhdWx0:default/true')
         pass
 
     def test_add(self, mock_adapter):
@@ -59,7 +55,7 @@ class Testiblox(unittest.TestCase):
         mock_adapter.post(requests_mock.ANY, json='zone_auth/ZG5zLnpvbmUkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3Q:unittest.example/default')
         result = self.iblox_conn.add(objtype='zone_auth', fqdn='unittest.example')
         self.assertTrue(isinstance(result, str))
-        self.assertEquals(result, 'zone_auth/ZG5zLnpvbmUkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3Q:unittest.example/default')
+        self.assertEqual(result, 'zone_auth/ZG5zLnpvbmUkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3Q:unittest.example/default')
         pass
 
     def test_add_host(self, mock_adapter):
@@ -68,7 +64,7 @@ class Testiblox(unittest.TestCase):
         mock_adapter.post(requests_mock.ANY, json='record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
         result = self.iblox_conn.add_host('testhost.unittest.example', '192.168.2.8', comment='Created by test_infoblox.py')
         self.assertTrue(isinstance(result, str))
-        self.assertEquals(result, 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
+        self.assertEqual(result, 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
         pass
 
     def test_add_alias(self, mock_adapter):
@@ -78,7 +74,7 @@ class Testiblox(unittest.TestCase):
         mock_adapter.put(requests_mock.ANY, json='record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
         result = self.iblox_conn.add_alias('testhost.unittest.example', 'testalias.unittest.example')
         self.assertTrue(isinstance(result, str))
-        self.assertEquals(result, 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
+        self.assertEqual(result, 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
         pass
 
     def test_delete_alias(self, mock_adapter):
@@ -88,7 +84,7 @@ class Testiblox(unittest.TestCase):
         mock_adapter.delete(requests_mock.ANY, json='record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
         result = self.iblox_conn.delete_alias('testhost.unittest.example', 'testalias.unittest.example')
         self.assertTrue(isinstance(result, dict))
-        self.assertEquals(result['_ref'], 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
+        self.assertEqual(result['_ref'], 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
         pass
 
     def test_delete_host(self, mock_adapter):
@@ -99,7 +95,7 @@ class Testiblox(unittest.TestCase):
         result = self.iblox_conn.get_host_by_name('testhost.unittest.example')[0]
         result = self.iblox_conn.delete(result['_ref'])
         self.assertTrue(isinstance(result, str))
-        self.assertEquals(result, 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
+        self.assertEqual(result, 'record:host/ZG5zLmhvc3QkLl9kZWZhdWx0LmV4YW1wbGUudW5pdHRlc3QudGVzdGhvc3Q:testhost.unittest.example/default')
         pass
 
 
